206b2e42095c45d13b37747a6e5e6a84bacf1870
jiyoungsophia <sophiathedev@gmail.com>
2024-08-11 07:47:09 +0900

    Chore: MainView 버튼 그리기

--- Beaver/Scenes/Main/MainView.swift
+++ Beaver/Scenes/Main/MainView.swift
@@ -7,47 +7,123 @@
 
 import SwiftUI
 import MapKit
+import CoreLocation
 
 struct MainView: View {
     @StateObject var locationManager = LocationManager()
+    @StateObject var warningManager = WarningManager()
     @StateObject var potHoleDataManager = PotHoleDataManager()
     
     @State private var cameraPosition: MapCameraPosition = .region(MKCoordinateRegion(
-        center: .hico,
+        center: CLLocationCoordinate2D.hico,
         span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
     ))
     
     @State private var selectedPothole: PotholeData?
     @State private var showPotholeInfo = false
     @State private var searchText = ""
+    @State var coordinates: Coordinates?
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            Map(position: $cameraPosition) {
-                ForEach(potHoleDataManager.hicoPotHoles, id: \.id) { pothole in
-                    Annotation("", coordinate: pothole.coordinates) {
-                        Image(pothole.averageDangerLevel?.rawValue ?? DangerLevel.low.rawValue)
-                            .resizable()
-                            .scaledToFit()
-                            .onTapGesture {
-                                selectedPothole = pothole
-                                showPotholeInfo = true
-                            }
+        NavigationStack {
+            ZStack{
+                Map(position: $cameraPosition) {
+                    ForEach(potHoleDataManager.hicoPotHoles, id: \.id) { pothole in
+                        Annotation("", coordinate: pothole.coordinates) {
+                            Image(pothole.averageDangerLevel?.rawValue ?? DangerLevel.low.rawValue)
+                                .resizable()
+                                .scaledToFit()
+                                .onTapGesture {
+                                    selectedPothole = pothole
+                                    showPotholeInfo = true
+                                }
+                        }
+                    }
+                }
+                .edgesIgnoringSafeArea(.all)
+                
+                VStack{
+                    SearchBarView(searchText: $searchText)
+                        .padding(.top, 24)
+                    buttonStack
+                    if showPotholeInfo, let pothole = selectedPothole {
+                        PotholeInfoView(pothole: pothole)
+                            .transition(.move(edge: .bottom))
+                            .animation(.spring(), value: showPotholeInfo)
+                            .padding(.bottom, 20)
                     }
                 }
             }
-            .edgesIgnoringSafeArea(.all)
-            
-            // PotholeInfoView
-            if showPotholeInfo, let pothole = selectedPothole {
-                PotholeInfoView(pothole: pothole)
-                    .transition(.move(edge: .bottom))
-                    .animation(.spring(), value: showPotholeInfo)
-                    .padding(.bottom, 20)
+            .toolbar(.hidden)
+            .navigationDestination(for: String.self){ destination in
+                if destination == "report"{
+                    ReportView(coordinates: $coordinates)
+                }
+            }
+            .onAppear {
+                print("auth")
+                Task{
+                    await warningManager.checkNotificationPermission()
+                }
+                locationManager.getLocationPermission()
+                potHoleDataManager.loadCSV()
+            }
+            .onChange(of: locationManager.currentLocation){
+                if let currentLocation = locationManager.currentLocation{
+                    for potholeCoordinate in potHoleDataManager.hicoPotHoles {
+                        warningManager.distanceToPotHole = CLLocationCoordinate2D.distance(from: currentLocation, to: potholeCoordinate.coordinates)
+                        print("distance: \(warningManager.distanceToPotHole)")
+                        if warningManager.showAlert{
+                            if warningManager.distanceToPotHole <= 30 && warningManager.distanceToPotHole > 20{
+                                warningManager.playWarningAudio(warningType: .threeHundred)
+                                Task{
+                                    await warningManager.notifyUser(title: "Beaver", content: WarningType.threeHundred.backgroundWarningContent)
+                                }
+                            }else if warningManager.distanceToPotHole <= 20 && warningManager.distanceToPotHole > 10{
+                                warningManager.playWarningAudio(warningType: .hundred)
+                                Task{
+                                    await warningManager.notifyUser(title: "Beaver", content: WarningType.hundred.backgroundWarningContent)
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        .onAppear {
-            potHoleDataManager.loadCSV()
+    }
+}
+
+extension MainView{
+    var buttonStack: some View{
+        VStack(alignment: .trailing){
+            HStack{
+                Spacer()
+                VStack{
+                    NavigationLink(value: "report"){
+                        Image("icon_report")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 65)
+                    }
+                    Button{
+                        warningManager.showAlert.toggle()
+                    }label:{
+                        Image("icon_start")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 65)
+                    }
+                    Spacer()
+                    Button{
+                        //다시 현재 위치로
+                    }label:{
+                        Image("icon_currentLocation")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 65)
+                    }
+                }.padding(.trailing, 24)
+            }
         }
     }
 }
