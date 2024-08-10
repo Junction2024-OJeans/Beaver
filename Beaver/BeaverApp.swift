8365d219b711513c8166bf2c8ef3b3e82906d5cb
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-08-11 00:29:29 +0900

    Initial Commit

---
+++ Beaver/BeaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaverApp.swift
+//  Beaver
+//
+//  Created by Jia Jang on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
