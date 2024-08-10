006176bd610e822694c5f721a9e143e1053ba095
Hyun Lee (Mia) <leeseunghyun7799@gmail.com>
2024-08-11 08:23:36 +0900

    그냥 이것 저것... alertView 연동 완

---
+++ Beaver/Scenes/Main/AlertView.swift
@@ -0,0 +1,18 @@
+//
+//  AlertView.swift
+//  Beaver
+//
+//  Created by Hyun Lee on 8/11/24.
+//
+
+import SwiftUI
+
+struct AlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertView()
+}
